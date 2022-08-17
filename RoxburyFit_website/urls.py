a2ddd142b80e711d0ebfda505339fc15d8586be3
Peter <ruecktenwald@gmail.com>
2022-08-16 20:02:31 -0400

    setup homepage

--- RoxburyFit_website/urls.py
+++ RoxburyFit_website/urls.py
@@ -2,8 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
+    path('', TemplateView.as_view(template_name="index.html"), name="index"),
 ]
--- RoxburyFit_website/views.py
+++ RoxburyFit_website/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Homepage")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({}, request))
