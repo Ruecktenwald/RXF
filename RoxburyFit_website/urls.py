449923b0ccf160737baf945ac6be7b962763abea
Peter <ruecktenwald@gmail.com>
2022-08-16 20:36:33 -0400

    create hypertext links between navbar pages

--- RoxburyFit_website/urls.py
+++ RoxburyFit_website/urls.py
@@ -2,9 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html"), name="index"),
+    path('', views.index, name="index"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
 ]
--- RoxburyFit_website/views.py
+++ RoxburyFit_website/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 def index(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render({}, request))
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
