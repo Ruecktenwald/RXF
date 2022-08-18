b2aac1ab52e11c945ffd3e782e67b42deb01a79a
Peter <ruecktenwald@gmail.com>
2022-08-17 22:13:41 -0400

    add login and create superuser

--- RoxburyFit_website/urls.py
+++ RoxburyFit_website/urls.py
@@ -1,12 +1,20 @@
 
 from django.contrib import admin
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
 
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('about', views.about, name="about"),
     path('contact', views.contact, name="contact"),
+
+    #Django Auth
+    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name = 'login'),
+
+
+
 ]
+
