9dd4529d3b6ea83ef0230fed1480d09b00601249
Peter <ruecktenwald@gmail.com>
2022-08-17 23:20:39 -0400

    add logout url

--- RoxburyFit_website/urls.py
+++ RoxburyFit_website/urls.py
@@ -10,10 +10,11 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('about', views.about, name="about"),
     path('contact', views.contact, name="contact"),
+    path('accounts/profile', views.ProfileView.as_view(), name= 'profile'),
 
     #Django Auth
     path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name = 'login'),
-
+    path('accounts/logout', auth_views.LogoutView.as_view(), name = 'logout'),
 
 
 ]
