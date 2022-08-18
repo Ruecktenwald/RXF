c900b1ceb59d3b46fc63f0115be211e711f71217
Peter <ruecktenwald@gmail.com>
2022-08-17 23:24:53 -0400

    add LoginRequiredMixin with new ProfileView class

--- RoxburyFit_website/views.py
+++ RoxburyFit_website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     return render(request, 'index.html')
@@ -8,3 +10,6 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = "accounts/profile.html"
