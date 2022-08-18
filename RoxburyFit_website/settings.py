0d9e5078ca37a45e5c56390369b972d7f7e396bc
Peter <ruecktenwald@gmail.com>
2022-08-17 22:34:29 -0400

    create login

--- RoxburyFit_website/settings.py
+++ RoxburyFit_website/settings.py
@@ -113,3 +113,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = "login"
+LOGIN_REDIRECT_URL = "index"
