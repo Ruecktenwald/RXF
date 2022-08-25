9c051d5d3056b074b6f81e272b8a42a5535a8fcb
Peter <ruecktenwald@gmail.com>
2022-08-25 12:09:28 -0400

    create user and persona model

--- RoxburyFit_website/apps/accounts/models.py
+++ RoxburyFit_website/apps/accounts/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 
+class UserPersona(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    normalize_name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.cascade, related_name="profile")
 
     is_full_name_displayed = models.BooleanField(default=True)
 
-#edit to test git
+
+    # details
+    bio = models.CharField(max_length=500, blank=True, null=True)
+    website= models.URLField(max_length=200, blank=True, null=True)
+    persona = models.ForeignKey(UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
+
