dfd38f4fe3f26af07e6002dc04138e692c0996a5
Peter <ruecktenwald@gmail.com>
2022-08-26 00:15:51 -0400

    add UserInterest model and migrate

--- RoxburyFit_website/apps/accounts/models.py
+++ RoxburyFit_website/apps/accounts/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class UserInterest(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    normalized_name = models.CharField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
 class UserPersona(models.Model):
     name = models.CharField(max_length=64, unique=True)
     normalized_name = models.CharField(max_length=64, unique=True)
@@ -11,14 +21,15 @@ class UserPersona(models.Model):
 
 
 class UserProfile(models.Model):
-
+    # owner
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
 
+    # settings
     is_full_name_displayed = models.BooleanField(default=True)
 
     # details
     bio = models.CharField(max_length=500, blank=True, null=True)
     website = models.URLField(max_length=200, blank=True, null=True)
     persona = models.ForeignKey(
-        UserPersona, on_delete=models.SET_NULL, blank=True, null=True
-    )
+        UserPersona, on_delete=models.SET_NULL, blank=True, null=True)
+    interests = models.ManyToManyField(UserInterest, blank=True)
