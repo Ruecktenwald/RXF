5a42fdf1fdd499fed59f16726c03136225677324
Peter <ruecktenwald@gmail.com>
2022-08-25 23:49:32 -0400

    fixed missing work.. - created persona model, user model and migration

--- RoxburyFit_website/urls.py
+++ RoxburyFit_website/urls.py
@@ -7,5 +7,5 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("RoxburyFit_website.apps.public.urls")),
-    path("accounts/", include("RoxburyFit_website.apps.accounts.urls"), name="profile"),
+    path("accounts/", include("RoxburyFit_website.apps.accounts.urls")),
 ]
